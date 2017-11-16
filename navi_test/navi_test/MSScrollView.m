e4cf9c63714de70ba67429d22e85bac2665ac155
liufei <liufei@fhkg.com>
2017-11-16 09:51:24 +0800

    优化

--- navi_test/navi_test/MSScrollView.m
+++ navi_test/navi_test/MSScrollView.m
@@ -23,29 +23,11 @@
     
     //    NSLog(@"tracking====%d===dragging====%d====decelerating===%d",self.tracking,self.dragging,self.decelerating);
     if (gestureRecognizer == self.panGestureRecognizer) {
-//        CGPoint point = [self.panGestureRecognizer velocityInView:self];
-//        if (point.x > 0) {
-//            return YES;
-//        }else{
-//            NSIndexPath *indexpath = self.indexPathsForVisibleItems.firstObject;
-//            MSRiskTopicViewController *vc = (MSRiskTopicViewController *)self.ms_viewController;
-//            RiskInfo *riskInfo = vc.dataArr[indexpath.item];
-//            if (riskInfo.isCompeleted) {
-//                if (self.dragging && self.decelerating) {
-//                    return NO;
-//                }else{
-//                    return YES;
-//                }
-//            }else{
-//                return NO;
-//            }
-//        }
         if (self.dragging && self.decelerating) {
             return NO;
         }else{
             return YES;
         }
-        
     }
     return YES;
 }
