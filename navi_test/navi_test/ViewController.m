7e7eee103d48c083ba543ee4d19c773cd25ab807
liufei <liufei@fhkg.com>
2017-11-29 15:03:03 +0800

    优化

--- navi_test/navi_test/ViewController.m
+++ navi_test/navi_test/ViewController.m
@@ -57,19 +57,19 @@ JSExportAs(onZMCallBack, - (void)onZMCallBack:(NSString *)params sign:(NSString 
 
 - (void)addSubveiws {
     
-    self.view.backgroundColor = [UIColor whiteColor];
-    self.navigationItem.title = @"MSWebViewJSExport";
-
-    self.webView = [[MSWebView alloc] initWithFrame:self.view.bounds];
-    self.webView.delegate = self;
-    self.webView.opaque = NO;
-    self.webView.jsObject = self;
-    self.webView.backgroundColor = [UIColor clearColor];
-    [self.view addSubview:self.webView];
-
-    NSURL *url = [NSURL URLWithString:MJCREDIT_DOWNLOAD_URL];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [self.webView loadRequest:request];
+//    self.view.backgroundColor = [UIColor whiteColor];
+//    self.navigationItem.title = @"MSWebViewJSExport";
+//
+//    self.webView = [[MSWebView alloc] initWithFrame:self.view.bounds];
+//    self.webView.delegate = self;
+//    self.webView.opaque = NO;
+//    self.webView.jsObject = self;
+//    self.webView.backgroundColor = [UIColor clearColor];
+//    [self.view addSubview:self.webView];
+//
+//    NSURL *url = [NSURL URLWithString:MJCREDIT_DOWNLOAD_URL];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [self.webView loadRequest:request];
     
 //    UICollectionView;
 //    UICollectionViewFlowLayout;
@@ -80,44 +80,44 @@ JSExportAs(onZMCallBack, - (void)onZMCallBack:(NSString *)params sign:(NSString 
 //
 //    CATransaction;
     
-//    self.automaticallyAdjustsScrollViewInsets = NO;
-//
-//    UIButton *btn1 = [[UIButton alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 60)];
-//    btn1.backgroundColor = [UIColor redColor];
-//    [btn1 setTitle:@"MSCardView1" forState:UIControlStateNormal];
-//    btn1.tag = 1000;
-//    [self.view addSubview:btn1];
-//    [btn1 addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
-//
-//    UIButton *btn2 = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(btn1.frame)+30, self.view.bounds.size.width, 60)];
-//    btn2.backgroundColor = [UIColor redColor];
-//    [btn2 setTitle:@"MSCardView2" forState:UIControlStateNormal];
-//    btn2.tag = 1001;
-//    [self.view addSubview:btn2];
-//    [btn2 addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
-//
-//    UIButton *btn3 = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(btn2.frame)+30, self.view.bounds.size.width, 60)];
-//    btn3.backgroundColor = [UIColor redColor];
-//    [btn3 setTitle:@"MSCardView3" forState:UIControlStateNormal];
-//    btn3.tag = 1002;
-//    [self.view addSubview:btn3];
-//    [btn3 addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+
+    UIButton *btn1 = [[UIButton alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 60)];
+    btn1.backgroundColor = [UIColor redColor];
+    [btn1 setTitle:@"MSCardView1" forState:UIControlStateNormal];
+    btn1.tag = 1000;
+    [self.view addSubview:btn1];
+    [btn1 addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
+
+    UIButton *btn2 = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(btn1.frame)+30, self.view.bounds.size.width, 60)];
+    btn2.backgroundColor = [UIColor redColor];
+    [btn2 setTitle:@"MSCardView2" forState:UIControlStateNormal];
+    btn2.tag = 1001;
+    [self.view addSubview:btn2];
+    [btn2 addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
+
+    UIButton *btn3 = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(btn2.frame)+30, self.view.bounds.size.width, 60)];
+    btn3.backgroundColor = [UIColor redColor];
+    [btn3 setTitle:@"MSCardView3" forState:UIControlStateNormal];
+    btn3.tag = 1002;
+    [self.view addSubview:btn3];
+    [btn3 addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
 }
 
-//- (void)tap:(UIButton *)btn {
-//    if (btn.tag == 1000) {
-//        ViewController1 *v1 = [ViewController1 new];
-//        [self.navigationController pushViewController:v1 animated:YES];
-//    } else if (btn.tag == 1001) {
-//        ViewController2 *v2 = [ViewController2 new];
-//        [self.navigationController pushViewController:v2 animated:YES];
-//    } else if (btn.tag == 1002) {
-//        ViewController3 *v3 = [ViewController3 new];
-//        [self.navigationController pushViewController:v3 animated:YES];
-//    } else {
-//
-//    }
-//}
+- (void)tap:(UIButton *)btn {
+    if (btn.tag == 1000) {
+        ViewController1 *v1 = [ViewController1 new];
+        [self.navigationController pushViewController:v1 animated:YES];
+    } else if (btn.tag == 1001) {
+        ViewController2 *v2 = [ViewController2 new];
+        [self.navigationController pushViewController:v2 animated:YES];
+    } else if (btn.tag == 1002) {
+        ViewController3 *v3 = [ViewController3 new];
+        [self.navigationController pushViewController:v3 animated:YES];
+    } else {
+
+    }
+}
 
 
 #pragma mark - UIWebViewDelegate
